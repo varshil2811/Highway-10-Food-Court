6891c94694d79bcebdb6384e075d5f5fe3fd71cf
varshil <varshilparejiya2004@gmail.com>
2026-08-11 12:39:29 +0530

    fix: express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use('/api/auth', authRouter)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')))
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   })
 }
